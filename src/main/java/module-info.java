91115ed87047b2ab5f0a40da548e4cdc22e30054
LauraMunozUAH <l.m.centenera@gmail.com>
2025-04-13 14:13:02 +0200

    logs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,17 @@
 module com.example.pcydpracticafinal {
     requires javafx.controls;
     requires javafx.fxml;
-    //requires javafx.web;
+    requires javafx.web;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    //requires net.synedra.validatorfx;
+    requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    //requires eu.hansolo.tilesfx;
-    //requires com.almasb.fxgl.all;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
     requires java.desktop;
+    requires org.apache.logging.log4j;
 
     opens com.example.pcydpracticafinal to javafx.fxml;
     exports com.example.pcydpracticafinal;
